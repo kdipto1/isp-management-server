de46421d900bc76a43a837816e25cd7880243848
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2023-10-15 23:47:58 +0600

    Auth middleware function added

--- src/app/modules/internetPackage/internetPackage.routes.ts
+++ src/app/modules/internetPackage/internetPackage.routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { InternetPackageController } from './internetPackage.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
@@ -7,6 +9,10 @@ router.post('/', InternetPackageController.create);
 router.get('/', InternetPackageController.getAllOrFilter);
 router.get('/:id', InternetPackageController.getById);
 router.patch('/:id', InternetPackageController.updateById);
-router.delete('/:id', InternetPackageController.deleteById);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  InternetPackageController.deleteById,
+);
 
 export const InternetPackageRoutes = router;
