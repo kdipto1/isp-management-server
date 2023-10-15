50dd01ff534bc383e876a5a4af8094b5f0adbfb1
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2023-10-15 21:03:21 +0600

    Review create,update,get,delete - routes,controller,service added

---
+++ src/app/modules/review/review.controller.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import { ReviewService } from './review.service';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
+
+const create = catchAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.create(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Review Created Successfully!',
+    data: result,
+  });
+});
+
+const getAllOrFilter = catchAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.getAllOrFilter();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Reviews Retrieved Successfully!',
+    data: result,
+  });
+});
+
+const getById = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ReviewService.getById(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Review Retrieved Successfully!',
+    data: result,
+  });
+});
+
+const updateById = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ReviewService.updateById(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Review Updated Successfully!',
+    data: result,
+  });
+});
+
+const deleteById = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await ReviewService.deleteById(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Review Deleted Successfully!',
+    data: result,
+  });
+});
+
+export const ReviewController = {
+  create,
+  getAllOrFilter,
+  getById,
+  updateById,
+  deleteById,
+};
---
+++ src/app/modules/review/review.service.ts
@@ -0,0 +1,51 @@
+import { Prisma } from '@prisma/client';
+import prisma from '../../../shared/prisma';
+
+const create = async (payload: Prisma.ReviewCreateInput) => {
+  const result = await prisma.review.create({
+    data: payload,
+  });
+  return result;
+};
+
+const getAllOrFilter = async () => {
+  const result = await prisma.review.findMany({});
+
+  return result;
+};
+
+const getById = async (id: string) => {
+  const result = await prisma.review.findUnique({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+const updateById = async (id: string, payload: Prisma.ReviewUpdateInput) => {
+  const result = await prisma.review.update({
+    where: {
+      id,
+    },
+    data: payload,
+  });
+  return result;
+};
+
+const deleteById = async (id: string) => {
+  const result = await prisma.review.delete({
+    where: {
+      id,
+    },
+  });
+  return result;
+};
+
+export const ReviewService = {
+  create,
+  getAllOrFilter,
+  getById,
+  updateById,
+  deleteById,
+};
